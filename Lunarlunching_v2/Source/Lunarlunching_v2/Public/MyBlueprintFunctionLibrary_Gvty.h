eac99166e20db5b68b2908132325cb39c70f656f
conormc <conor96@hotmail.co.uk>
2024-01-28 12:49:37 +0000

    adding gravity
    adding gravity to all the objects along with glowing oven and growing cookie

---
+++ Lunarlunching_v2/Source/Lunarlunching_v2/Public/MyBlueprintFunctionLibrary_Gvty.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MyBlueprintFunctionLibrary_Gvty.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUNARLUNCHING_V2_API UMyBlueprintFunctionLibrary_Gvty : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
---
+++ Lunarlunching_v2/Source/Lunarlunching_v2/Public/MyWorldSettings.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/WorldSettings.h"
+#include "MyWorldSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUNARLUNCHING_V2_API AMyWorldSettings : public AWorldSettings
+{
+	GENERATED_BODY()
+
+	
+};
